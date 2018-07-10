f89e1e609c347f4104a9bbe0afdccb725df6bb94
Andrei Mihai <andrei_mihai@waters.com>
2018-07-10 17:17:15 +0300

    Add test Fixture

---
+++ Server/Basilicum.Server.Test/Helpers/SliceFixture.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Basilicum.Server.Test.Helpers
+{
+	public class SliceFixture
+	{
+		private readonly IServiceScopeFactory scopeFactory;
+		public SliceFixture()
+		{
+			var services = new ServiceCollection();
+			var provider = services.BuildServiceProvider();
+			scopeFactory = provider.GetService<IServiceScopeFactory>();
+		}
+
+		public async Task<TResponse> Send<TResponse>(IRequest<TResponse> request)
+		{
+			using (var scope = scopeFactory.CreateScope())
+			{
+				var mediator = scope.ServiceProvider.GetService<IMediator>();
+				return await mediator.Send(request);
+			}
+		}
+	}
+}
--- Server/Basilicum.Server.Test/UnitTest1.cs
+++ Server/Basilicum.Server.Test/UnitTest1.cs
@@ -1,13 +1,22 @@
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Basilicum.Server.Test
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
-    }
+	[TestClass]
+	public class UnitTest1
+	{
+		
+
+		[TestMethod]
+			public void TestMethod1()
+			{
+				using (var scope = scopeFactory.CreateScope())
+				{
+					var mediator = scope.ServiceProvider.GetService<IMediator>();
+					return mediator.Send(request);
+				}
+			}
+	}
 }
