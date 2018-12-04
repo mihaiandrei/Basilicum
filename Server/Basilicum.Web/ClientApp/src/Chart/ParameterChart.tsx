43bc4c10c8ccfb4b6e40d1315f390cc8a451a7b3
Andrei Mihai <andrei_mihai@waters.com>
2018-12-04 16:41:25 +0200

    add axis to chart

--- Server/Basilicum.Web/ClientApp/src/Chart/ParameterChart.tsx
+++ Server/Basilicum.Web/ClientApp/src/Chart/ParameterChart.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { VictoryChart, VictoryLine } from "victory";
+import { VictoryAxis, VictoryChart, VictoryLine, VictoryScatter } from "victory";
 import * as React from 'react';
 import IMeasurementModel from '../Measurement/MeasurementModel';
 import IDateTimeValueChartItem from './DateTimeValueChartItem'
 
 interface IState {
-    measurements: IDateTimeValueChartItem[];
+    chartDataItems: IDateTimeValueChartItem[];
 }
 interface IProps {
     parameterId: number | null
@@ -14,18 +14,18 @@ interface IProps {
 class ParameterChart extends React.Component<IProps, IState>{
     constructor(props: IProps) {
         super(props);
-        this.state = { measurements: [] };
-        this.handleTextChange();
+        this.state = { chartDataItems: [] };
+        this.loadMeasurement();
     }
 
-    public handleTextChange = () => {
+    public loadMeasurement = () => {
 
         axios.get(`http://localhost:1200/api/mesurement/list?ParameterId=3&StartDate=1%2F1%2F2018&EndDate=12%2F12%2F2018`)
             .then(res => {
 
-                const chartItems = res.data.map((item: IMeasurementModel) => ({ x: item.date, y: item.value } as IDateTimeValueChartItem));
+                const chartItems = res.data.map((item: IMeasurementModel) => ({ x: new Date(item.date), y: item.value } as IDateTimeValueChartItem));
                 this.setState({
-                    measurements: chartItems
+                    chartDataItems: chartItems
                 });
             })
     }
@@ -33,14 +33,22 @@ class ParameterChart extends React.Component<IProps, IState>{
     public render() {
         return (
             <div>
-                <VictoryChart>
-                    <VictoryLine
-                        data={this.state.measurements}
+                <VictoryChart width={600} height={470} scale={{ x: "time" }}>
+                    <VictoryAxis tickFormat={this.tickFormatYear}
+                        tickCount={3}
+                    />
+                    <VictoryAxis dependentAxis={true} />
+                    <VictoryLine data={this.state.chartDataItems}
+                                interpolation="linear" />
+                    <VictoryScatter style={{ data: { fill: 'red' } }}
+                                    size={2}
+                                    data={this.state.chartDataItems}
                     />
                 </VictoryChart>
             </div>
         );
     }
+    private tickFormatYear = (x: Date) =>x.toLocaleString();
 }
 
 export default ParameterChart;
