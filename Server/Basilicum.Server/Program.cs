b58d71800d0ee0eafb9f8d86b9825a61516b681c
Andrei Mihai <andrei_mihai@waters.com>
2019-01-15 15:35:57 +0200

    enable app insights

--- Server/Basilicum.Server/Program.cs
+++ Server/Basilicum.Server/Program.cs
@@ -13,6 +13,7 @@
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseApplicationInsights()
                 .Build();
     }
 }
