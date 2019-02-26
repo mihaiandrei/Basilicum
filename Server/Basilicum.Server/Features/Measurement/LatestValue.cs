28d0922556783830e6ee8dc250851d296f68f479
Andrei Mihai <andrei_mihai@waters.com>
2019-02-26 15:24:40 +0200

    Latest version returns full model

--- Server/Basilicum.Server/Features/Measurement/LatestValue.cs
+++ Server/Basilicum.Server/Features/Measurement/LatestValue.cs
@@ -1,34 +1,48 @@
 ﻿namespace Basilicum.Server.Features.Measurement
 {
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
     using Basilicum.Server.Infrastructure;
     using MediatR;
     using Microsoft.EntityFrameworkCore;
+    using System;
     using System.Linq;
     using System.Threading;
     using System.Threading.Tasks;
 
     public class LatestValue
     {
-        public class Query : IRequest<double?>
+        public class Query : IRequest<Model>
         {
             public int ParameterId { get; set; }
         }
 
-        public class QueryHandler : IRequestHandler<Query, double?>
+        public class Model
+        {
+            public int Id { get; set; }
+            public double Value { get; set; }
+            public DateTime Date { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, Model>
         {
             private readonly DatabaseContext context;
-
-            public QueryHandler(DatabaseContext context)
+            private readonly IConfigurationProvider configurationProvider;
+            public QueryHandler(DatabaseContext context,
+                                IConfigurationProvider configurationProvider)
             {
                 this.context = context;
+                this.configurationProvider = configurationProvider;
             }
 
-            public async Task<double?> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Model> Handle(Query request, CancellationToken cancellationToken)
             {
                 var measurement = await context.Measurement
-                                    .OrderByDescending(m => m.Date)
-                                    .FirstOrDefaultAsync(m => m.ParameterId == request.ParameterId);
-                return measurement?.Value;
+                                                .Where(m => m.ParameterId == request.ParameterId)
+                                                .ProjectTo<Model>(configurationProvider)
+                                                .OrderByDescending(m => m.Date)
+                                                .FirstOrDefaultAsync();
+                return measurement;
             }
         }
     }
