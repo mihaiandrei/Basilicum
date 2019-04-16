c887cbef046581eb8ccfe09b7af782da88321b45
Andrei Mihai <andrei_mihai@waters.com>
2019-04-16 15:47:28 +0300

    Add date picker

--- Server/Basilicum.Web/ClientApp/src/Chart/ParameterChart.tsx
+++ Server/Basilicum.Web/ClientApp/src/Chart/ParameterChart.tsx
@@ -1,15 +1,22 @@
 import { VictoryAxis, VictoryChart, VictoryLine, VictoryScatter, VictoryTooltip } from "victory";
 import * as React from 'react';
 import IParameterChartData from './ParameterChartData';
+import DatePicker from 'react-datepicker'
+import "react-datepicker/dist/react-datepicker.css";
 
 interface IProps {
     chartData: IParameterChartData[]
 }
 
-class ParameterChart extends React.Component<IProps>{
+interface IState {
+    startDate: Date;
+}
+
+class ParameterChart extends React.Component<IProps, IState>{
     constructor(props: IProps) {
         super(props);
-    }  
+        this.state = { startDate: new Date() };
+    }
 
     public render() {
         const lines = this.props.chartData.map((item) => {
@@ -20,32 +27,48 @@ class ParameterChart extends React.Component<IProps>{
 
         const points = this.props.chartData.map((item) => {
             return (
-                <VictoryScatter 
+                <VictoryScatter
                     style={{ data: { fill: 'green' } }}
                     size={2}
                     data={item.data}
                     key={item.id}
                     labels={(d) => d.y}
-                    labelComponent={<VictoryTooltip/>}
+                    labelComponent={<VictoryTooltip />}
                 />
             );
         });
 
         return (
             <div>
-                <VictoryChart width={600} height={470} scale={{ x: "time" }}>
-                    <VictoryAxis 
-                        tickFormat={this.tickFormatYear}
-                        tickCount={2}
-                    />
-                    {lines}
-                    {points}
-                    <VictoryAxis dependentAxis={true} />
-                </VictoryChart>
+                <div>
+                    <VictoryChart width={600} height={470} scale={{ x: "time" }}>
+                        <VictoryAxis
+                            tickFormat={this.tickFormatYear}
+                            tickCount={2}
+                        />
+                        {lines}
+                        {points}
+                        <VictoryAxis dependentAxis={true} />
+                    </VictoryChart>
+                </div>
+                <div className="container">
+                    <div className="react-datepicker-wrapper">
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleStartDateChange}
+                            showTimeSelect={true}
+                            timeFormat="HH:mm"
+                            timeIntervals={15}
+                            timeCaption="time"
+                            placeholderText="Start date" />
+                    </div>
+                </div>
             </div>
         );
     }
     private tickFormatYear = (x: Date) => x.toLocaleString();
+    private handleStartDateChange = (date: Date) => { this.setState({ startDate: date }) };
+
 }
 
 export default ParameterChart;
